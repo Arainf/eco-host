847306300d0d78b0c8424cb937c7028c07bf41df
Arainf <adrianrainierf@gmail.com>
2025-11-24 14:39:59 +0800

    Ecohost Version 2.0.2 FIX

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers =
+      Request::HEADER_X_FORWARDED_FOR |
+      Request::HEADER_X_FORWARDED_HOST |
+      Request::HEADER_X_FORWARDED_PORT |
+      Request::HEADER_X_FORWARDED_PROTO |
+      Request::HEADER_X_FORWARDED_AWS_ELB;
+
+    public function handle($request, \Closure $next)
+    {
+        // Force HTTPS if behind proxy
+        if ($request->header('X-Forwarded-Proto') === 'https') {
+            URL::forceScheme('https');
+        }
+
+        return parent::handle($request, $next);
+    }
+}
